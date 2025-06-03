5bcc591c6b07e17e68c674cfdc058e3d5c67bec7
LeeChan <ion0203@naver.com>
2025-06-04 00:53:51 +0900

    Initial Commit

--- app/src/main/java/mjc/example/healthplanner/Planner.java
+++ app/src/main/java/mjc/example/healthplanner/Planner.java
@@ -1,19 +1,12 @@
 package mjc.example.healthplanner;
 
-import android.graphics.Color;
+
 import android.os.Bundle;
-import android.text.style.ForegroundColorSpan;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.prolificinteractive.materialcalendarview.*;
-import com.prolificinteractive.materialcalendarview.spans.DotSpan;
 
-import org.threeten.bp.DayOfWeek;
-import org.threeten.bp.LocalDate;
-
-import java.util.Calendar;
 public class Planner extends AppCompatActivity {
 
     MaterialCalendarView calendarView;
