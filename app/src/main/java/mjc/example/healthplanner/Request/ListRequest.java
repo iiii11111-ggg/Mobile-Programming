5375a208d1be7100b9e76e4c81132722126400f9
LeeChan <ion0203@naver.com>
2025-06-05 15:59:12 +0900

    Ok update

---
+++ app/src/main/java/mjc/example/healthplanner/Request/ListRequest.java
@@ -0,0 +1,22 @@
+package mjc.example.healthplanner.Request;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ListRequest extends StringRequest {
+    final static private String URL = "http://172.111.79.223:8080/UserLogin.jsp"; //
+    private Map<String,String> parameters;
+    public ListRequest(String userID,String exerciseId, Response.Listener<String> listener) {
+        super(Method.POST, URL, listener, null);
+        parameters = new HashMap<>();
+        parameters.put("userID", userID);
+        parameters.put("exerciseId",exerciseId);
+    }
+    @Override
+    public Map<String, String> getParams() {
+        return parameters;
+    }
+}
